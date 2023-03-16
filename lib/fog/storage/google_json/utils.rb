68b7d699979af6a0510f9abe0ad3aaf60ecc2d3d
Ian Hollander <ianholla@gmail.com>
2023-03-16 08:50:40 -0400

    Preserve + in path

--- lib/fog/storage/google_json/utils.rb
+++ lib/fog/storage/google_json/utils.rb
@@ -20,7 +20,7 @@ module Fog
         def host_path_query(params, expires)
           params[:headers]["Date"] = expires.to_i
           # implementation from CGI.escape, but without ' ' to  '+' conversion
-          params[:path] = params[:path].b.gsub(/([^a-zA-Z0-9_.\-~]+)/) { |m|
+          params[:path] = params[:path].b.gsub(/([^a-zA-Z0-9_.\-+~]+)/) { |m|
             '%' + m.unpack('H2' * m.bytesize).join('%').upcase
           }.gsub("%2F", "/")
 
